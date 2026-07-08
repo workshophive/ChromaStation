65f72b849f1988c95d233d1df15b31f119454049
workshophive <shadowbiller333@gmail.com>
2026-07-08 18:49:50 -0400

    im going to kms. nevermind to the night vision system

--- Content.Goobstation.Shared/Overlays/SharedThermalVisionSystem.cs
+++
@@ -1,5 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace Content.Goobstation.Shared.Overlays;
-
-public sealed class SharedThermalVisionSystem : SwitchableOverlaySystem<ThermalVisionComponent, ToggleThermalVisionEvent>;
--- Content.Goobstation.Shared/SpecialAnimation/SpecialAnimationViewerComponent.cs
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-namespace Content.Goobstation.Shared.SpecialAnimation;
-
-/// <summary>
-/// All entities with this component can see the most epic animations in their lives.
-/// </summary>
-[RegisterComponent]
-public sealed partial class SpecialAnimationViewerComponent : Component;
